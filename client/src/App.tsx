3e43367f4649527269e05ca0b849e2564243537d
Rajanan <g.rajan1993@gmail.com>
2024-01-09 10:45:43 +0100

    chore(client): move files in client folder

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import TestInfo from "./components/organisms/resume";
+import TestForm from "./components/organisms/testForm";
+
+const App = () => {
+  return (
+    <main className="container mx-auto my-10">
+      <TestInfo />
+      <TestForm />
+    </main>
+  );
+};
+export default App;
