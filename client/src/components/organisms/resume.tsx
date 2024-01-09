278d574e582f7204b0ca7a42ef97bc2bed305292
Rajanan <g.rajan1993@gmail.com>
2024-01-09 10:46:24 +0100

    feat(organism/resume): components show project info

---
+++ client/src/components/organisms/resume.tsx
@@ -0,0 +1,35 @@
+
+const TestInfo = () =>
+    <section className="my-14">
+        <h1 className="my-5 text-5xl text-center">Test-Ayomi</h1>
+        <p className="my-5">Exercice 1 :
+            Créer un algorithme qui réalise une calculatrice en notation polonaise inverse (NPI), le tout en Python.
+            Développer une API REST avec la bibliothèque web python Fast API. Cette api permettra d’envoyer les opérations à réaliser et retournera
+            le résultat.
+        </p>
+        <ul>
+            <li>
+                Enregistrer sur une base de données les opérations et les résultats.
+            </li>
+            <li>
+                Faire une route API qui permet de récupérer dans un fichier Csv les datas.
+            </li>
+            <li>
+                Déployer le projet sur docker à l’aide de docker-compose.
+            </li>
+            <li>
+                Exercice Plus :
+            </li>
+            <li>
+                Documentation
+            </li>
+            <li>
+                Unitest
+            </li>
+            <li>
+                Frontend pour la calculatrice en React
+            </li>
+        </ul>
+    </section>
+
+export default TestInfo
