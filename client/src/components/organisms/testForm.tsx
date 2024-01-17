2af5e657c29db49f473b40b0b8d2125702965929
Rajanan <g.rajan1993@gmail.com>
2024-01-17 18:15:01 +0100

    feat(client): download file

--- client/src/components/organisms/testForm.tsx
+++ client/src/components/organisms/testForm.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import Calculatrice from "./calculatrice";
+import Buttons from "../atoms/button";
+import FileSaver from 'file-saver';
 
 const TestForm = () => {
     const [state, setState] = useState<string>('');
@@ -42,12 +44,14 @@ const TestForm = () => {
         setResult(response.result)
     }
 
-    // const getCsv = async () => {
-    //     const response = await fetch('http://localhost:8000/csv').then(data => data.json())
-    // }
+    const getCsv = async () => {
+        const response = await fetch('http://localhost:8000/csv').then(data => data.blob())
+        FileSaver.saveAs(response, 'operation.csv');
+    }
 
     return (
         <section className="flex flex-col items-center justify-center bg-gray-900">
+            <Buttons onClick={() => getCsv()}>Télécharger liste des opérations</Buttons>
             <p className="text-white">exemple: 3 10 5 + * ou 10 5 + 3 *</p>
             <p className="p-3 my-10 text-5xl bg-white">{operation.join(' ')} {result ? `= ${result}` : ''}</p>
             {/* <p className="text-xl text-red-500">{message}</p> */}
